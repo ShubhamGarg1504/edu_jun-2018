503d875bdc9bf9ae227bbe1fdc4e2f0cae5658b5
shubhu <sayamagg@gmail.com>
2019-10-20 11:33:17 +0530

    Fibonacci java program executed

--- Fibona/Fibonacci.java
+++ Fibona/Fibonacci.java
@@ -1,25 +1,24 @@
-package Edureka;
-import java.util.Scanner;
-public class Fibonacci {
+class Fibonacci
+{  
+public static void main(String args[])  
+
+{    
+ int n1=0,n2=1,n3,i,count=10;    
+
+ System.out.print(n1+" "+n2);  
+    
+ 
+for(i=2;i<count;++i)   
+ {    
+  n3=n1+n2;    
   
-    public static void main(String[] args) 
-    {
-     
-         int n, first = 0,next = 1;
-          
-            System.out.println("Enter how may fibonnaci numbers to print");
-            Scanner scanner = new Scanner(System.in);
-            n = scanner.nextInt();
-            System.out.print("The first " + n + " Fibonacci numbers are: ");
-            System.out.print(first + " " + next);
-            for (int i = 1; i<=n-2; ++i)
-            {
-                int sum = first + next;
-                first = next;
-                next = sum;
-                System.out.print(" " + sum);
-            }
+System.out.print(" "+n3);    
   
-    }
+n1=n2;    
   
-}
+n2=n3;    
+
+ }  
+  
+  
+}}
